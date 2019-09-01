8fee6d929b075a445fe721d5ab436f3695873f6d
komorowskidev <komorowskidev@gmail.com>
2019-09-01 19:47:16 +0200

    initial commit

---
+++ src/main/java/pl/komorowskidev/designpatternsjavaimplementation/ApplicationLauncher.java
@@ -0,0 +1,8 @@
+package pl.komorowskidev.designpatternsjavaimplementation;
+
+public class ApplicationLauncher {
+
+    public static void main(String[] args) {
+        System.out.println("Design patterns - java implementation");
+    }
+}
